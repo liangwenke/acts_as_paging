c2d1a2d3127671eef75ef02321773b5b58c6bce5
wenke <liangwenke8@gmail.com>
2010-06-07 00:23:35 +0800

    cleanup

--- init.rb
+++ init.rb
@@ -1,1 +1,4 @@
 #require 'acts_as_paging'
+ActiveRecord::Base.send(:include, ActsAsPaging)
+ActionController::Base.send(:include, ActsAsPaging::Support)
+#ApplicationHelper.send(:include, ActsAsPaging::Helper)
